933ee84c8919d95b4f1f6e6c1db590447f99cc67
charlie y <charles.young@student.unimelb.edu.au>
2025-10-31 17:26:51 +1100

    Updated: windows screenshots

--- tests/screenshots.spec.ts
+++ tests/screenshots.spec.ts
@@ -7,36 +7,43 @@ const screenshotOptions = {
 };
 test('Login Page Screenshot', async ({ page }) => {
   await page.goto("/signin");
+  await page.waitForLoadState('domcontentloaded');
   await expect(page).toHaveScreenshot(screenshotOptions);
 });
 
 test('Signup Page Screenshot', async ({ page }) => {
   await page.goto("/signup");
+  await page.waitForLoadState('domcontentloaded');
   await expect(page).toHaveScreenshot(screenshotOptions);
 });
 
 test('My-Rubrics Page Screenshot', async ({ page }) => {
   await page.goto("/my-rubrics");
+  await page.waitForLoadState('domcontentloaded');
   await expect(page).toHaveScreenshot(screenshotOptions);
 });
 
 test('Profile Page Screenshot', async ({ page }) => {
   await page.goto("/profile");
+  await page.waitForLoadState('domcontentloaded');
   await expect(page).toHaveScreenshot(screenshotOptions);
 });
 
 test('Templates Page Screenshot', async ({ page }) => {
   await page.goto("/templates");
+  await page.waitForLoadState('domcontentloaded');
   await expect(page).toHaveScreenshot(screenshotOptions);
 });
 
 test('Error Page Screenshot', async ({ page }) => {
   await page.goto("/error");
+  await page.waitForLoadState('domcontentloaded');
   await expect(page).toHaveScreenshot(screenshotOptions);
 });
 
 test('Goodbye Page Screenshot', async ({ page }) => {
   await page.goto("/goodbye");
+  await page.waitForLoadState('domcontentloaded');
   await expect(page).toHaveScreenshot(screenshotOptions);
 });
 
@@ -45,10 +52,12 @@ test('Goodbye Page Screenshot', async ({ page }) => {
 
 test('Edit-Rubric Page Screenshot', async ({ page }) => {
   await page.goto("/edit-rubric/68ec64b5-6401-4839-967b-09c5d07f4945");
+  await page.waitForLoadState('domcontentloaded');
   await expect(page).toHaveScreenshot(screenshotOptions);
 });
 
 test('Edit-Template Page Screenshot', async ({ page }) => {
   await page.goto("/edit-template/82e8cc53-4538-45bb-9558-2e5172ff8c5b");
+  await page.waitForLoadState('domcontentloaded');
   await expect(page).toHaveScreenshot(screenshotOptions);
 });
